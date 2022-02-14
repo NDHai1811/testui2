e4b12a5ab8431eeae5d3e07ea4fa6094b2327463
NDHai1811 <71643190+NDHai1811@users.noreply.github.com>
2022-02-14 09:44:03 +0700

    Initial commit

---
+++ app/src/main/java/com/example/testui2/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.testui2;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    CardView cardView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        cardView=findViewById(R.id.cardhd);
+        cardView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this,"Click",Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+}
